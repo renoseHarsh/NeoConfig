d1e6f598ee35005862220f5f65ad265bfc92ff8d
renoseHarsh <renoseharsh@gmail.com>
2024-09-14 18:12:48 +0530

    Inital Config

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    local term = require("toggleterm")
+    term.setup({
+      open_mapping = "<C-\\>"
+    })
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+
+      vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
+
+    vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+  end
+}
